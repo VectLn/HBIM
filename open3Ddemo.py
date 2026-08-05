b4b9341e83f6e3c51181af65b52d6e8e8746f533
Lan Kerlau <lkerlau@bradford.ac.uk>
2026-08-05 14:31:26 +0100

    open3Ddemo plus pratique

--- open3Ddemo.py
+++ open3Ddemo.py
@@ -1,9 +1,17 @@
-#cd C:\dev\open3D
+import argparse
 import open3d as o3d
 
-dataset = o3d.data.PCDPointCloud()
-pcd = o3d.io.read_point_cloud("Paris5.ply")
 
-o3d.io.write_point_cloud("output.ply", pcd, write_ascii=True)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_path")
+    parser.add_argument("-o", "--output", default="output.ply")
+    args = parser.parse_args()
 
-o3d.visualization.draw_geometries([pcd], window_name="Visualisation Open3D")
+    pcd = o3d.io.read_point_cloud(args.input_path)
+    o3d.io.write_point_cloud(args.output, pcd, write_ascii=True)
+    o3d.visualization.draw_geometries([pcd], window_name="Visualisation Open3D")
+
+
+if __name__ == "__main__":
+    main()
